0b1d2eea82d22449cb753a5f57f6a39c54595416
hinako <hinako222853@gmail.com>
2021-06-18 21:37:10 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
             
   def index
       @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
   end
 
   def new
@@ -28,12 +28,27 @@ class TasksController < ApplicationController
   end
 
   def update
+
+    if task.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+
+      flash[:success] = 'タスクは正常に削除されました'
+    redirect_to taskss_url
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
