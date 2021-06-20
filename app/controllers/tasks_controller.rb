1c8477945e50dcfceb1e89e1028e742ace92b614
hinako <hinako222853@gmail.com>
2021-06-20 19:06:43 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   def destroy
 
       flash[:success] = 'タスクは正常に削除されました'
-    redirect_to taskss_url
+    redirect_to tasks_url
   end
   
   private
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
