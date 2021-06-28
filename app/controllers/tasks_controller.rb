6296223b63c34e44eb8152829d56f2a5c91d7aa0
hinako <hinako222853@gmail.com>
2021-06-28 22:04:46 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
             
   def index
-      @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -14,12 +14,12 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
-      
+      @task = current_user.tasks.build(task_params)
       if @task.save
           flash[:success] = 'タスクが正常に送信されました'
-          redirect_to @task
+          redirect_to root_url
       else
+          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
           flash.now[:danger] = "タスクが送信されませんでした"
           render:new
       end
@@ -29,7 +29,6 @@ class TasksController < ApplicationController
   end
 
   def update
-
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -43,7 +42,7 @@ class TasksController < ApplicationController
       @task.destroy
 
       flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+      redirect_back(fallback_location: root_url)
   end
   
   private
@@ -54,6 +53,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
